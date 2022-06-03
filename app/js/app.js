3e73c07ca10a48f79a5b2745528447e92ba2d5e0
ш Артём <serg-turbo-rybin@mail.ru>
2022-06-03 09:14:34 +0300

    buttons style changed

--- app/js/app.js
+++ app/js/app.js
@@ -14,33 +14,33 @@ document.addEventListener('DOMContentLoaded', () => {
 	// } 
 
 
-	const whiteButtons = document.querySelectorAll('.white-button');
-	for (let whiteButton of whiteButtons) {
-		document.addEventListener('click', whiteButtonActivate)
+	// const whiteButtons = document.querySelectorAll('.white-button');
+	// for (let whiteButton of whiteButtons) {
+	// 	document.addEventListener('click', whiteButtonActivate)
 
-		function whiteButtonActivate(event) {
-			if (event.target.classList.contains('white-button')) {
-				event.target.classList.toggle('white-button-active')
-			} else {
-				whiteButton.classList.remove('white-button-active')
-			}
-		}
-	}
+	// 	function whiteButtonActivate(event) {
+	// 		if (event.target.classList.contains('white-button')) {
+	// 			event.target.classList.toggle('white-button-active')
+	// 		} else {
+	// 			whiteButton.classList.remove('white-button-active')
+	// 		}
+	// 	}
+	// }
 
 
-	 const redButtons = document.querySelectorAll('.red-button');
+	//  const redButtons = document.querySelectorAll('.red-button');
 
-	 for (let redButton of redButtons) {	
-	 	document.addEventListener('click', redButtonActivate)
+	//  for (let redButton of redButtons) {	
+	//  	document.addEventListener('click', redButtonActivate)
 
-	 	function redButtonActivate(event) {
-	 		if (event.target.classList.contains('red-button')) {
-	 			event.target.classList.add('red-button-active')
-	 		} else {
-	 			redButton.classList.remove('red-button-active')
-	 		}
-	 	}
-	 }
+	//  	function redButtonActivate(event) {
+	//  		if (event.target.classList.contains('red-button')) {
+	//  			event.target.classList.add('red-button-active')
+	//  		} else {
+	//  			redButton.classList.remove('red-button-active')
+	//  		}
+	//  	}
+	//  }
 
     
 
